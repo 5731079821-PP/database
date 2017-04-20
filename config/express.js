5d09762206c526e048359b2e42dbbe7d47e9ce1a
PATCHARIDA PUDPADEE <calojy@hotmail.com>
2017-04-20 16:29:35 +0700

    Done Login

--- config/express.js
+++ config/express.js
@@ -1,6 +1,10 @@
 var express=require('express');
+//var bodyParser=require('body-parser');
 module.exports=function(){
   var app=express();
-  require('../app/routes/login.routes')(app);
+  app.set('views','./app/views');
+  app.set('view engine','jade');
+  require('../app/routes/login.routes')(app);//at runtime
+  app.use(express.static('./public')); //order imp
   return app;
 }
